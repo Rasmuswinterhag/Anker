eedafcbac910597597116be3df67244d4536d30f
ZuperZlime <Rasmuswinterhag@gmail.com>
2025-01-17 17:12:47 +0100

    Update MinimumSpeed.cs

--- Assets/Scripts/MinimumSpeed.cs
+++ Assets/Scripts/MinimumSpeed.cs
@@ -16,7 +16,7 @@ public class MinimumSpeed : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         speed = rb.velocity.magnitude;
 
